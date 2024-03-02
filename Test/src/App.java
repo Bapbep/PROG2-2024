c96df8e0ce7a6bbfc26fe7d151914157083cee4f
Bapbep <2969737952@qq.com>
2024-03-02 12:27:31 +0800

    My first java

---
+++ Test/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Let's get it done!");
+        System.out.println("Hello world!");
+    }
+}
